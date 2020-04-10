6c0a33ffd337f85810cab8df930c75781d11aaa4
Tate DeVito <tcd2tq@virginia.edu>
2020-04-10 13:15:58 -0400

    Basic ProfileCard added

--- fitness-app/src/WorkoutScreen/WorkoutScreen.tsx
+++ fitness-app/src/WorkoutScreen/WorkoutScreen.tsx
@@ -1,28 +1,54 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button, Avatar } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Card,
+  CardContent,
+  CardMedia
+} from "@material-ui/core";
 
 export default function WorkoutScreen() {
   // A profile bar has a name of user, and an avatar in the top corner
-  function ProfileBar(props: { name: string }) {
+  function NavBar(props: { name: string }) {
     return (
       <div>
         <AppBar color="inherit">
           <Toolbar>
             <Button color="secondary">Login</Button>
             <Typography color="secondary" variant="h6">
-              <b>Welcome {props.name} !</b>
+              <b>Welcome!</b>
             </Typography>
-            <Avatar>{props.name[0]}</Avatar>
           </Toolbar>
         </AppBar>
       </div>
     );
   }
 
+  function ProfileCard(props: { name: string }) {
+    return (
+      <div>
+        <Card>
+          <CardContent>
+            <Typography component="h2">
+              <b>{props.name}</b>
+            </Typography>
+            <Typography component="p">
+              Favorite Sports: Surfing, Soccer
+            </Typography>
+          </CardContent>
+          {/* TODO: Avatar? */}
+          <CardMedia component="img" />
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <ProfileBar name="Tate" />
-      Hi!
+      <NavBar name="Tate" />
+      <ProfileCard name="Tate" />
     </div>
   );
 }
