7167262b44ded9938ee18ff714ac62e913093c32
Tate DeVito <tcd2tq@virginia.edu>
2020-05-03 17:01:45 -0400

    Refactor Routine and addWorkout button

--- fitness-app/src/WorkoutScreen/Routine/Routine.tsx
+++ fitness-app/src/WorkoutScreen/Routine/Routine.tsx
@@ -1,41 +1,50 @@
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Fab } from "@material-ui/core";
 import Workout from "./Workout/Workout";
+import AddIcon from "@material-ui/icons/Add";
 
 export default function Routine() {
   //TODO: Has "workout pages" built from a queue of workouts
+  //TODO: Should have a current workout - the current should be greatly highlighted,
+  // and you can scroll through the others
   //TODO: Should have a progress bar to show how far along the length it is
 
   // TODO: This has to be an array of Workouts specifically
-  const [workoutQueue, setWorkoutQueue] = useState<Object[]>([{}, {}, {}]);
+  const [workoutQueue, setWorkoutQueue] = useState<Object[]>([{}]);
 
-  const workoutSlider = () => {
-    let workoutRow;
-    if (workoutQueue.length > 0) {
-      workoutRow = (
-        <>
-          {workoutQueue.map((workout) => (
-            <Grid item>
-              {/* TODO: Should be automatic */}
-              <Workout />
-            </Grid>
-          ))}
-        </>
-      );
-    }
+  const workoutRow = (
+    <Grid
+      container
+      spacing={1}
+      wrap="nowrap"
+      alignContent="center"
+      style={{ overflow: "auto" }}
+    >
+      {workoutQueue.map((workout) => (
+        <Grid item>
+          <Workout />
+        </Grid>
+      ))}
+    </Grid>
+  );
 
-    return (
-      <Grid
-        container
-        spacing={1}
-        wrap="nowrap"
-        alignContent="center"
-        style={{ overflow: "auto" }}
-      >
-        {workoutRow}
-      </Grid>
-    );
-  };
+  const addButton = (
+    <Grid item spacing={1}>
+      <Fab size="medium" color="secondary">
+        <AddIcon
+          // TODO: expand onClick functionality; possibly add general add workout function
+          onClick={() => {
+            setWorkoutQueue([...workoutQueue, {}]);
+          }}
+        />
+      </Fab>
+    </Grid>
+  );
 
-  return <>{workoutSlider()}</>;
+  return (
+    <Grid container spacing={2}>
+      {workoutRow}
+      {addButton}
+    </Grid>
+  );
 }
--- fitness-app/src/WorkoutScreen/Routine/Workout/Workout.tsx
+++ fitness-app/src/WorkoutScreen/Routine/Workout/Workout.tsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import {
-  Card,
-  CardContent,
   Checkbox,
-  Fab,
-  IconButton,
   List,
   ListItem,
+  ListItemSecondaryAction,
+  IconButton,
   Paper,
   Typography,
 } from "@material-ui/core";
-import { Edit, Delete } from "@material-ui/icons";
-import AddIcon from "@material-ui/icons/Add";
+import { Delete } from "@material-ui/icons";
 
 export default function Workout() {
   const [name, setName] = useState<string>("Workout Name");
@@ -20,22 +17,9 @@ export default function Workout() {
     { name: "Bench Press" },
     { name: "Pull Ups" },
   ]);
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const [addMode, setAddMode] = useState<boolean>(false);
-  // TODO: location?
 
   // This should be broken out to another component, and then exercises should
   function ExerciseList() {
-    let deleteButton = <div />;
-
-    if (editMode) {
-      deleteButton = (
-        <IconButton size="small">
-          <Delete />
-        </IconButton>
-      );
-    }
-
     return (
       <List>
         {/* Maps exercises to the list */}
@@ -44,48 +28,31 @@ export default function Workout() {
           <ListItem divider={true}>
             <Typography component="p">{exercise.name}</Typography>
             <Checkbox />
-            {/* TODO: If this gets clicked, the state containing exercises must be 
+            <ListItemSecondaryAction>
+              <IconButton>
+                {/* TODO: If this gets clicked, the state containing exercises must be 
             adjusted so that this exercise is not in it */}
-            {deleteButton}
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
           </ListItem>
         ))}
       </List>
     );
   }
 
-  const EditToggle = () => {
-    setEditMode(!editMode);
+  // TODO: This should add an exercise to the end of the workout
+  function AddExercise(exercise: string) {
     return;
-  };
-
-  const AddToggle = () => {
-    setAddMode(!addMode);
-    return;
-  };
-
-  function AddExercise() {
-    // TODO: Make it so this adds an exercise
-    // Idea 1 --> have two options of adding an exercise; this actually may be too much work,
-    // Idea 2 --> Maybe this button just has an add button, and other workouts seen below have an add button also
-    // Third idea, maybe just when you hit the add button, automatically enable these options
-    return <div />;
   }
 
   return (
     <div>
       <Paper>
-        {/* TODO: Determine if this is needed when add feature is looked at further */}
-        {/* <IconButton size="small" onClick={EditToggle}>
-            <Edit />
-          </IconButton> */}
         <Typography component="h2">{name}</Typography>
         {/* TODO: Make name, and exercise list editable */}
         <ExerciseList />
-        <Fab size="small" color="primary" onClick={AddToggle}>
-          <AddIcon />
-        </Fab>
       </Paper>
-      <AddExercise />
     </div>
   );
 }
